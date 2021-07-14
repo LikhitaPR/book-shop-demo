d96a073aeff2fcbbc2641a3eef21db412da5fe62
Likhita Phutane <plikhita@healthedge.com>
2021-07-14 15:35:04 +0530

    changed credentials for deployment

---
+++ src/main/java/com/tw/bootcamp/bookshop/checkstyle/CheckStyleApp.java
@@ -0,0 +1,14 @@
+package com.tw.bootcamp.bookshop.checkstyle;
+
+public class CheckStyleApp {
+    private CheckStyleApp() {
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, CheckStyle");
+    }
+}
